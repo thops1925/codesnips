5d6722fa73acbc05996d4edfe91d47e852804ade
ThopsD <74445064+thops1925@users.noreply.github.com>
2023-06-25 23:30:16 +0800

    save

--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -2,17 +2,25 @@ import Feed from '@app/components/Feed';
 import { desc } from '@lib/desc';
 import logo from '@public/assets/images/thops3.png';
 import Image from 'next/image';
+import Loading from './loading';
+import { Suspense } from 'react';
+import { PrismaClient } from '@prisma/client';
 
-async function getData() {
-	const res = await fetch('http://localhost:3000/api/prompt', { cache: 'no-store' });
-	if (!res.ok) {
-		throw new Error('Failed to fetch data');
-	}
-	return res.json();
-}
+const prisma = new PrismaClient();
+export const runtime = 'edge'; // 'nodejs' (default) | 'edge'
+
+// async function getData() {
+// 	const res = await fetch('http://localhost:3000/api/prompt', { cache: 'no-store' });
+// 	if (!res.ok) {
+// 		throw new Error('Failed to fetch data');
+// 	}
+// 	return res.json();
+// }
 
 const Home = async () => {
-	const data: Post[] = await getData();
+	const data = await prisma.prompt.findMany({
+		include: { creator: true },
+	});
 
 	return (
 		<section className='flex flex-col items-center justify-center w-full'>
@@ -20,7 +28,9 @@ const Home = async () => {
 				<h1 className='text-normal text-center font-mono tracking-wider text-gray-800 '>{desc}</h1>
 				<Image src={logo} alt='logo' className='object-contain blur-0' blurDataURL='data:...' placeholder='blur' />
 			</div>
-			<Feed data={data} />
+			<Suspense fallback={<Loading />}>
+				<Feed data={data} />
+			</Suspense>
 		</section>
 	);
 };
